0cd7064fc1ab1eb3781c3070ce38a40b22e5bc3f
Your Name <mari@Maribels-MacBook-Pro.local>
2022-03-10 20:37:15 -0600

    second go

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const server = require('./api/server');
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
-    console.log(process.env.MY_ENV);
     console.log(`server is up on port ${PORT}, user is ${process.env.USER}!`);
 });
