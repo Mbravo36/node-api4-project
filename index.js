40842742b2386d5c69e0761204b52c986c92c048
Your Name <mari@Maribels-MacBook-Pro.local>
2022-03-10 19:52:36 -0600

    have basics in

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 9000;
+
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.end('<h1> hello world! </h1>')
+});
+
+server.listen(PORT, () => {
+    console.log(process.env.MY_ENV);
+    console.log(`server is up on port ${PORT}, user is ${process.env.USER}!`);
+});
